fe347343dfaf6af25c353676097fa7cd3f8499f7
Ikhsan230103 <zg74720@gmail.com>
2024-07-11 09:19:58 +0700

    slider

--- app/Views/landingpage/landingpage.php
+++ app/Views/landingpage/landingpage.php
@@ -2,4 +2,46 @@
 
 <?= $this->section('content') ?>
 
+<div class="slider">
+    <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="row">
+                    <div class="col-md-6 slider-description">
+                        <h2>ERIA Supports ASEAN Chairmanship</h2>
+                        <p>ERIA collaborates closely with the ASEAN Member State holding the Chairmanship to design and deliver Priority Economic Deliverables, providing essential support in this process.</p>
+                        <a href="#" class="btn btn-outline-light">Read More</a>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="<?= base_url('assets/images/flags.jpeg') ?>" class="d-block w-100" alt="Slide 1">
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="row">
+                    <div class="col-md-6 slider-description">
+                        <h2>Participate in the ASEAN Economic Community Post-2025 Survey!</h2>
+                        <p>ERIA in collaboration with the ASEAN Secretariat has developed a survey focusing on AEC Aspects of Post-2025 Vision. This survey,
+                            supported by Deutsche Gesellschaft für Internationale Zusammenarbeit GmbH (GIZ), aims to capture the general sentiments and perspectives of ASEAN stakeholders.
+                            It seeks to engage a wider audience to contribute to the End-Term Review of the AEC Blueprint 2025 and the development of the AEC Community Vision 2045 and its Strategic Plan.</p>
+                        <a href="#" class="btn btn-outline-light">Read More</a>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="<?= base_url('assets/images/web-link.jpg') ?>" class="d-block w-100" alt="Slide 2">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>
+
 <?= $this->endSection(); ?>
--- app/Views/main.php
+++ app/Views/main.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/responsive.css')?>">
-    <link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/style.css')?>">
-    <link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/font-awesome.css')?>">
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/responsive.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/style.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/font-awesome.css') ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
+
 <body>
 
-<div class="navbar">
+    <div class="navbar">
         <div class="nav-logo">
             <img src="<?= base_url('assets/images/logo-eria.png') ?>" alt="">
         </div>
@@ -23,11 +26,32 @@
                 <li><a href="#">PUBLICATIONS</a></li>
                 <li><a href="#">UPDATES</a></li>
                 <li><a href="#">EVENTS</a></li>
+                <li>
+                    <button class="btn btn-primary btn-search" data-bs-toggle="modal" data-bs-target="#searchModal">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </li>
             </ul>
         </div>
     </div>
 
-
+    <!-- Modal -->
+    <div class="modal fade" id="searchModal" tabindex="-1" aria-labelledby="searchModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <form action="<?= base_url('search') ?>" method="get">
+                        <div class="input-group">
+                            <input type="text" class="form-control search-input" id="searchQuery" name="query" placeholder="Search...">
+                            <button class="btn btn-primary search-btn" type="submit"><i class="fas fa-search"></i> Search</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    
     <?= $this->renderSection('content') ?>
 </body>
+
 </html>
