4cf2c42439f92c30bab8bdf1ade1724e345be130
AryoSyawal <aryosyawal.bimo@gmail.com>
2024-07-17 10:29:42 +0700

    charts added

--- app/Views/landingpage/landingpage.php
+++ app/Views/landingpage/landingpage.php
@@ -44,6 +44,37 @@
     </div>
 </div>
 
+<!----------------------------------------------------CHARTS---------------------------------------------------------->
+
+<div class="mt-5" style="width: 100%;">
+        <div class="row">
+            <div class="col-md-12 d-flex gap-2 justify-content-center">
+                <button class="btn btn-primary" id="showVolume">Show Volume</button>
+                <button class="btn btn-primary" id="showDemography">Show Demography</button>
+            </div>
+        </div>
+        <div class="row mt-1 d-flex justify-content-center">
+            <div class="col-md-8 w-100 p-5">
+            <div id="volumeContent" class="content-pane" style="width:100%;">
+                    <!-- Content for Demography -->
+                    <h4>Volume Section</h4>
+                    <div class="volumewrapper d-flex justify-content-evenly" style="gap: 40px; height:300px;">
+                    <canvas id="volumedonut1" style="max-width: 45%; height: 110px;"></canvas>
+                    <canvas id="volumedonut2" style="max-width: 45%; height: 110px;"></canvas>
+                    </div>
+                </div>
+                <div id="demographyContent" class="content-pane" style="display: none; width:100%;">
+                    <!-- Content for Demography -->
+                    <h4>Demography Section</h4>
+                    <div class="demowrapper d-flex justify-content-evenly" style="gap: 40px; height:300px;">
+                    <canvas id="demobar" style="max-width: 45%; height: 110px;"></canvas>
+                    <canvas id="demoline" style="max-width: 45%; height: 110px;"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 <!---------------------------------------------------------- UPDATE NEWS ---------------------------------------------------------->
 <div class="page-content mt-3 pt-1">
     <div class="container1">
@@ -113,5 +144,162 @@
         
     </div>
 </div>
+<?= $this->endSection(); ?>
 
+<?= $this->section('script') ?>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js" integrity="sha512-L0Shl7nXXzIlBSUUPpxrokqq4ojqgZFQczTYlGjzONGTDAcLremjwaWv5A+EDLnxhQzY5xUZPWLOLqYRkY0Cbw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+ var ctxDonut1 = document.getElementById('volumedonut1').getContext('2d');
+
+// Define the data for your dataset
+var dataDonut1 = {
+    labels: ['BEV', 'PHEV', 'H2', 'LPG', 'CNG', 'LNG'],
+    datasets: [{
+        label: 'Donut Chart 1',
+        backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)', 'rgb(75, 192, 192)', 'rgb(153, 102, 255)'],
+        borderColor: 'rgb(255, 255, 255)',
+        data: [70, 20, 10]
+    }]
+};
+
+// Configuration options
+var optionsDonut1 = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top',
+        },
+        tooltip: {
+            callbacks: {
+                label: function(tooltipItem) {
+                    return tooltipItem.label + ': ' + tooltipItem.raw.toFixed(2);
+                }
+            }
+        }
+    }
+};
+
+// Create the chart
+var myDonutChart1 = new Chart(ctxDonut1, {
+    type: 'doughnut',
+    data: dataDonut1,
+    options: optionsDonut1
+});
+
+
+
+
+
+var ctxDonut2 = document.getElementById('volumedonut2').getContext('2d');
+
+// Define the data for your dataset
+var dataDonut2 = {
+    labels: ['BEV', 'PHEV'],
+    datasets: [{
+        label: 'Donut Chart 2',
+        backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)', 'rgb(75, 192, 192)', 'rgb(153, 102, 255)'],
+        borderColor: 'rgb(255, 255, 255)',
+        data: [20, 80]
+    }]
+};
+
+// Configuration options
+var optionsDonut2 = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top',
+        },
+        tooltip: {
+            callbacks: {
+                label: function(tooltipItem) {
+                    return tooltipItem.label + ': ' + tooltipItem.raw.toFixed(2);
+                }
+            }
+        }
+    }
+};
+
+// Create the chart
+var myDonutChart2 = new Chart(ctxDonut2, {
+    type: 'doughnut',
+    data: dataDonut2,
+    options: optionsDonut2
+});
+</script>
+<script>
+        // Get the context of the canvas element we want to select
+        var ctxBar = document.getElementById('demobar').getContext('2d');
+        var ctxLine = document.getElementById('demoline').getContext('2d');
+
+
+        // Define the data for your dataset
+        var dataBar = {
+        labels: ['2020', '2021', '2022', '2023', '2024'],
+        datasets: [{
+            label: 'Sample Dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [10, 20, 30, 40, 50, 60, 70]
+        }]
+    };
+
+    var dataLine = {
+        labels: ['2020', '2021', '2022', '2023', '2024', '2025'],
+        datasets: [{
+            label: 'Dataset 1',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [60, 20, 60, 20, 60, 20, 80]
+        }, {
+            label: 'Dataset 2',
+            backgroundColor: 'rgb(54, 162, 235)',
+            borderColor: 'rgb(54, 162, 235)',
+            data: [50, 10, 50, 10, 50, 10, 70]
+        }]
+    };
+
+    // Configuration options
+    var options = {
+        responsive: true,
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    };
+
+    // Create the charts
+    var chartBar = new Chart(ctxBar, {
+        type: 'bar',
+        data: dataBar,
+        options: options
+    });
+
+    var chartLine = new Chart(ctxLine, {
+        type: 'line',
+        data: dataLine,
+        options: options
+    });
+    </script>
+
+<script>
+    $(document).ready(function() {
+        // Show Volume section
+        $('#showVolume').on('click', function() {
+            $('#demographyContent').hide(); // Hide Demography section
+            $('#volumeContent').show(); // Show Volume section
+        });
+        
+        // Show Demography section
+        $('#showDemography').on('click', function() {
+            $('#volumeContent').hide(); // Hide Volume section
+            $('#demographyContent').show();
+
+            // Show Demography section
+        });
+    });
+</script>
 <?= $this->endSection(); ?>
--- app/Views/main.php
+++ app/Views/main.php
@@ -12,6 +12,15 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <?= $this->renderSection('style') ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <style>
+        .content-pane {
+            padding: 20px;
+            border: 1px solid #ccc;
+            margin-top: 10px;
+        }
+    </style>
+    
 </head>
 
 <body>
@@ -52,6 +61,8 @@
     </div>
     
     <?= $this->renderSection('content') ?>
+
+    <?= $this->renderSection('script') ?>
     
 </body>
 
