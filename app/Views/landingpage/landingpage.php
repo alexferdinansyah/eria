c9a98f21b3aaddbf705156a8bb0aefef55b785e7
Ikhsan230103 <zg74720@gmail.com>
2024-07-22 10:57:52 +0700

    road

--- app/Views/landingpage/landingpage.php
+++ app/Views/landingpage/landingpage.php
@@ -34,12 +34,15 @@
         </div>
         <div class="card-drop dropdown-card d-flex justify-content-center">
             <div class="dropdown-container">
-                <div class="select-wrapper">
-                    <p>View interactive infographics containing up-to-date statistics on infrastructures and fleet data for road vehicles, private and commercial</p>
-                    <p class="mt-1">Available for all ASEAN countries</p>
-                    <div class="logo-dropdown d-flex align-items-center">
+                <div class="select-wrapper d-flex align-items-center">
+                    <div class="dropdown-logo-container">
                         <img src="<?= base_url('assets/images/road.png') ?>" alt="Logo" class="dropdown-logo">
-                        <div class="dropdown-wrapper">
+                        <h4 class="dropdown-title">Road</h4>
+                    </div>
+                    <div class="ml-3">
+                        <p class="mb-0">View interactive infographics containing up-to-date statistics on infrastructures and fleet data for road vehicles, private and commercial</p>
+                        <p class="mt-1 mb-0">Available for all ASEAN countries</p>
+                        <div class="dropdown-content mt-3">
                             <select class="dropdown-country transparent-dropdown" id="asean-dropdown" onchange="redirectToSummary()">
                                 <option disabled hidden selected value="country">CHOOSE COUNTRY</option>
                                 <option value="Brunei">Brunei</option>
@@ -53,14 +56,16 @@
                                 <option value="Thailand">Thailand</option>
                                 <option value="Vietnam">Vietnam</option>
                             </select>
-                            <div class="dropdown-arrow">&#9662;</div> <!-- Dropdown arrow -->
+                            <div class="dropdown-arrow">&#9662;</div>
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
 </div>
+
 
 <!---------------------------------------------------------- UPDATE NEWS ---------------------------------------------------------->
 <h3 style="margin-left:100px;">Updates</h3>
@@ -70,55 +75,55 @@
             <img src="https://www.eria.org//uploads/articles/20150119/eria_capacity-building_phnom-penh_jan-19-20-cropped.jpg" alt="Capacity Building">
             <a href="#">ERIA Capacity Building Seminar in Cambodia - Remaining Challenge for ASEAN Economic Community 2015</a>
             <p style="margin-bottom:0;">12 Juli 2024</p>
-            <p class="news-p" >Phnom Penh, 4 July 2024: ERIA President Tetsuya Watanabe paid a courtesy visit to Cambodia’s Deputy Prime Minister and Minister of Economy and Finance, H.E. Dr Aun Pornmoniroth.</p>
+            <p class="news-p">Phnom Penh, 4 July 2024: ERIA President Tetsuya Watanabe paid a courtesy visit to Cambodia’s Deputy Prime Minister and Minister of Economy and Finance, H.E. Dr Aun Pornmoniroth.</p>
         </div>
         <div class="col-md-4 grid-item">
             <img src="https://www.eria.org//uploads/articles/20140812/cb-ha-noi-2014-crop.jpg" alt="Capacity Building">
-            <a href="#"> Regional Knowledge Centre for Marine Plastic Debris                                    </a>
+            <a href="#"> Regional Knowledge Centre for Marine Plastic Debris </a>
             <p style="margin-bottom:0;">12 Juli 2024</p>
-            <p class="news-p" >Phnom Penh, 4 July 2024: ERIA President Tetsuya Watanabe paid a courtesy visit to Cambodia’s Deputy Prime Minister and Minister of Economy and Finance, H.E. Dr Aun Pornmoniroth.</p>
+            <p class="news-p">Phnom Penh, 4 July 2024: ERIA President Tetsuya Watanabe paid a courtesy visit to Cambodia’s Deputy Prime Minister and Minister of Economy and Finance, H.E. Dr Aun Pornmoniroth.</p>
         </div>
         <div class="col-md-4 grid-item">
             <img src="https://www.eria.org//uploads/news/20240708/eria-president-pays-courtesy-visit-to-cambodia-s-deputy-prime-minister-and-minister-of-economy-and-finance.webp" alt="Capacity Building">
             <a href="#">Capacity Building Programme</a>
             <p style="margin-bottom:0;">12 Juli 2024</p>
-            <p class="news-p" >Phnom Penh, 4 July 2024: ERIA President Tetsuya Watanabe paid a courtesy visit to Cambodia’s Deputy Prime Minister and Minister of Economy and Finance, H.E. Dr Aun Pornmoniroth.</p>
+            <p class="news-p">Phnom Penh, 4 July 2024: ERIA President Tetsuya Watanabe paid a courtesy visit to Cambodia’s Deputy Prime Minister and Minister of Economy and Finance, H.E. Dr Aun Pornmoniroth.</p>
         </div>
         <div class="col-md-4 grid-item">
             <img src="https://www.eria.org//uploads/articles/20140812/cb-ha-noi-2014-crop.jpg" alt="Capacity Building">
             <a href="#">Capacity Building Programme</a>
             <p style="margin-bottom:0;">12 Juli 2024</p>
-            <p class="news-p" >Phnom Penh, 4 July 2024: ERIA President Tetsuya Watanabe paid a courtesy visit to Cambodia’s Deputy Prime Minister and Minister of Economy and Finance, H.E. Dr Aun Pornmoniroth.</p>
+            <p class="news-p">Phnom Penh, 4 July 2024: ERIA President Tetsuya Watanabe paid a courtesy visit to Cambodia’s Deputy Prime Minister and Minister of Economy and Finance, H.E. Dr Aun Pornmoniroth.</p>
         </div>
         <div class="col-md-4 grid-item">
             <img src="https://www.eria.org//uploads/articles/20140812/cb-ha-noi-2014-crop.jpg" alt="Capacity Building">
             <a href="#">Capacity Building Programme</a>
             <p style="margin-bottom:0;">12 Juli 2024</p>
-            <p class="news-p" >Phnom Penh, 4 July 2024: ERIA President Tetsuya Watanabe paid a courtesy visit to Cambodia’s Deputy Prime Minister and Minister of Economy and Finance, H.E. Dr Aun Pornmoniroth.</p>
+            <p class="news-p">Phnom Penh, 4 July 2024: ERIA President Tetsuya Watanabe paid a courtesy visit to Cambodia’s Deputy Prime Minister and Minister of Economy and Finance, H.E. Dr Aun Pornmoniroth.</p>
         </div>
         <div class="col-md-4 grid-item">
             <img src="https://www.eria.org//uploads/articles/20140812/cb-ha-noi-2014-crop.jpg" alt="Capacity Building">
             <a href="#">Capacity Building Programme</a>
             <p style="margin-bottom:0;">12 Juli 2024</p>
-            <p class="news-p" >Phnom Penh, 4 July 2024: ERIA President Tetsuya Watanabe paid a courtesy visit to Cambodia’s Deputy Prime Minister and Minister of Economy and Finance, H.E. Dr Aun Pornmoniroth.</p>
+            <p class="news-p">Phnom Penh, 4 July 2024: ERIA President Tetsuya Watanabe paid a courtesy visit to Cambodia’s Deputy Prime Minister and Minister of Economy and Finance, H.E. Dr Aun Pornmoniroth.</p>
         </div>
         <div class="col-md-4 grid-item">
             <img src="https://www.eria.org//uploads/articles/20140812/cb-ha-noi-2014-crop.jpg" alt="Capacity Building">
             <a href="#">Capacity Building Programme</a>
             <p style="margin-bottom:0;">12 Juli 2024</p>
-            <p class="news-p" >Phnom Penh, 4 July 2024: ERIA President Tetsuya Watanabe paid a courtesy visit to Cambodia’s Deputy Prime Minister and Minister of Economy and Finance, H.E. Dr Aun Pornmoniroth.</p>
+            <p class="news-p">Phnom Penh, 4 July 2024: ERIA President Tetsuya Watanabe paid a courtesy visit to Cambodia’s Deputy Prime Minister and Minister of Economy and Finance, H.E. Dr Aun Pornmoniroth.</p>
         </div>
         <div class="col-md-4 grid-item">
             <img src="https://www.eria.org//uploads/articles/20140812/cb-ha-noi-2014-crop.jpg" alt="Capacity Building">
             <a href="#">Capacity Building Programme</a>
             <p style="margin-bottom:0;">12 Juli 2024</p>
-            <p class="news-p" >Phnom Penh, 4 July 2024: ERIA President Tetsuya Watanabe paid a courtesy visit to Cambodia’s Deputy Prime Minister and Minister of Economy and Finance, H.E. Dr Aun Pornmoniroth.</p>
+            <p class="news-p">Phnom Penh, 4 July 2024: ERIA President Tetsuya Watanabe paid a courtesy visit to Cambodia’s Deputy Prime Minister and Minister of Economy and Finance, H.E. Dr Aun Pornmoniroth.</p>
         </div>
         <div class="col-md-4 grid-item">
             <img src="https://www.eria.org//uploads/articles/20140812/cb-ha-noi-2014-crop.jpg" alt="Capacity Building">
             <a href="#">Capacity Building Programme</a>
             <p style="margin-bottom:0;">12 Juli 2024</p>
-            <p class="news-p" >Phnom Penh, 4 July 2024: ERIA President Tetsuya Watanabe paid a courtesy visit to Cambodia’s Deputy Prime Minister and Minister of Economy and Finance, H.E. Dr Aun Pornmoniroth.</p>
+            <p class="news-p">Phnom Penh, 4 July 2024: ERIA President Tetsuya Watanabe paid a courtesy visit to Cambodia’s Deputy Prime Minister and Minister of Economy and Finance, H.E. Dr Aun Pornmoniroth.</p>
         </div>
     </div>
 </section>
@@ -126,19 +131,19 @@
 <!---------------------------------------------------------- CHART SECTION  ---------------------------------------------------------->
 <section class="chart-container">
     <div class="chart-wrap">
-    <canvas id="myChart1"></canvas>
+        <canvas id="myChart1"></canvas>
     </div>
 
     <div class="chart-wrap">
-    <canvas id="myChart2"></canvas>
+        <canvas id="myChart2"></canvas>
     </div>
 
     <div class="chart-wrap">
-    <canvas id="myChart3"></canvas>
+        <canvas id="myChart3"></canvas>
     </div>
 
     <div class="chart-wrap">
-    <canvas id="myChart4"></canvas>
+        <canvas id="myChart4"></canvas>
     </div>
 </section>
 
@@ -153,6 +158,3 @@
 
 
 <?= $this->endSection(); ?>
-
-
-
