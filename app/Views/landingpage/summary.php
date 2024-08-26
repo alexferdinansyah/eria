0b3f5e49c34948b77c9b82b4f23072bf0f9e895c
dhykas <mdhika987@gmail.com>
2024-08-26 12:11:23 +0700

    eco & forecast adjust chart

--- app/Views/landingpage/summary.php
+++ app/Views/landingpage/summary.php
@@ -10,7 +10,7 @@
 
 <div class="row">
     <div class="summary-wrapper">
-        <!-- summary -->
+        
         <h1 style="margin-left:50px;">Econometric</h1>
         <div class="grid row row-cols-2 justify-content-evenly gap-0 column-gap-3 row-gap-3 mb-4">
             <div class="content-summary">
@@ -21,11 +21,11 @@
                         <p><?= $population ?></p>
                     </div>
                     <div>
-                        <p>Total Land Area - km</p>
+                        <p>Total Land Area - km²</p>
                         <p><?= $ttlland ?></p>
                     </div>
                     <div>
-                        <p>Total Highway Area - km</p>
+                        <p>Total Highway Area - km²</p>
                         <p><?= $totalHighway ?></p>
                     </div>
                 </section>
@@ -61,42 +61,12 @@
             <div class="chart-parent" style="width: 40%;padding-left:30px !important;">
                 <canvas class="chartdiv" id="line2"></canvas>
             </div>
-            <div class="d-flex align-items-center gap-2" style="width: 40%;padding-left:50px !important;">
-                <div class="chart-parent" style="width: 300px;">
-                    <canvas class="chartdiv" id="myChart4"></canvas>
-                </div>
-                <ul>
-                    <li>VOL ICE : <?= $volIce ?></li>
-                    <li>VOL HEV : <?= $volHev ?></li>
-                    <li>VOL BEV : <?= $volBev ?></li>
-                    <li>VOL PHEV : <?= $volPhev ?></li>
-                </ul>
-            </div>
 
-            <!-- bar chart -->
-            <div class="chart-parent" style="width: 40%;padding-left:15px !important;">
-                <canvas class="chartdiv" id="bar1"></canvas>
-            </div>
-            <div class="chart-parent" style="width: 40%;">
-                <canvas class="chartdiv" id="bar2"></canvas>
-            </div>
-            <div class="chart-parent" style="width: 40%;padding-left:30px !important;">
-                <canvas class="chartdiv" id="bar3"></canvas>
-            </div>
-            <div class="chart-parent" style="width: 40%;padding-left:20px !important;">
-                <canvas class="chartdiv" id="bar4"></canvas>
-            </div>
-            <div class="chart-parent" style="width: 40%;padding-left:20px !important;">
-                <canvas class="chartdiv" id="bar5"></canvas>
-            </div>
-            <div class="chart-parent" style="width: 40%;padding-left:20px !important;">
-                <canvas class="chartdiv" id="bar6"></canvas>
-            </div>
 
-            <div class="chart-parent" style="width: 40%;">
-            </div>
+            
         </div>
-        <h1 style="margin-left:50px;">Historical</h1>
+
+        <h1 style="margin-top:150px; margin-left:50px;">Historical</h1>
         <div class="grid row row-cols-2 justify-content-evenly gap-0 column-gap-3 row-gap-3 mb-4">
             <div class="content-summary">
                 <h2>Summary</h2>
@@ -182,42 +152,9 @@
             <div class="chart-parent" style="width: 40%;">
             </div>
         </div>
-        <h1 style="margin-left:50px;">Forecasting</h1>
+
+        <h1 style="margin-top:150px; margin-left:50px;">Forecasting</h1>
         <div class="grid row row-cols-2 justify-content-evenly gap-0 column-gap-3 row-gap-3 mb-4">
-            <div class="content-summary">
-                <h2>Summary</h2>
-                <section class="container-sum">
-                    <div>
-                        <p>Population</p>
-                        <p><?= $population ?></p>
-                    </div>
-                    <div>
-                        <p>Total Land Area - km</p>
-                        <p><?= $ttlland ?></p>
-                    </div>
-                    <div>
-                        <p>Total Highway Area - km</p>
-                        <p><?= $totalHighway ?></p>
-                    </div>
-                </section>
-                <section class="container-sum">
-                    <div>
-                        <p>GDP Per Capita - US$</p>
-                        <p><?= $GDP ?></p>
-                    </div>
-                    <div>
-                        <p>Purchasing Power Parity (PPP) - US$</p>
-                        <p><?= $PPP ?></p>
-                    </div>
-                </section>
-                <section class="container-sum">
-                    <div>
-                        <p>GDP Per Capita Purchasing Power Parity (PPP) - US$</p>
-                        <p><?= $GDPppp ?></p>
-                    </div>
-                </section>
-            </div>
-            <img class="img-map" src="<?= base_url('assets/images/nations/maps/'  . $nation . ".svg") ?>" alt="">
 
             <!-- chart top -->
             <div class="chart-parent" style="width: 40%;">
@@ -319,27 +256,3 @@
 
 <?= $this->section('script') ?>
 <?= $this->endSection(); ?>
-
-<!-- all chart -->
-<section class="chart-container p-3 mb-5">
-    <div class="chart-wrap row-gap-3 column-gap-2 gap-0 row row-cols-1 justify-content-md-evenly">
-        <div class="volume d-flex flex-column">
-            <!-- <a href="https://docs.google.com/spreadsheets/d/1zucgTdbeN9ZbrUcYMu2R_LXtSobx1saYwAXdEbz79Qg/export?format=xlsx&gid=0"><button style="width: 12%; margin-bottom: 30px; margin-left: 30px;" class="btn btn-primary">Download Volume</button> -->
-            </a>
-            <div class="volume-chart-wrapper d-flex justify-content-evenly">
-                <!-- <div class="chartdiv" id="linechart1"></div> -->
-                <!-- <div id="donutchart" class="chartdiv"></div> -->
-            </div>
-        </div>
-
-        <div class="demograpy d-flex flex-column">
-            <!-- <a href="https://docs.google.com/spreadsheets/d/1zucgTdbeN9ZbrUcYMu2R_LXtSobx1saYwAXdEbz79Qg/export?format=xlsx&gid=11352702"><button style="width: 14%; margin-bottom: 30px; margin-left: 30px; margin-top: 20px;" class="btn btn-primary">Download Demography</button> -->
-            </a>
-            <div class="demography-chart-wrapper d-flex justify-content-evenly">
-                <!-- <div class="chartdiv" id="linechart3" ></div> -->
-                <!-- <div class="chartdiv" id="linechart2"></div> -->
-            </div>
-        </div>
-
-    </div>
-</section>
